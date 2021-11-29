9847012d574c2b7aed182641492c3cc8b6d3112d
Alc2110 <38523363+Alc2110@users.noreply.github.com>
2021-11-29 21:40:24 +1100

    add configuration class

---
+++ src/MyLibrary/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    static class Configuration
+    {
+        // database connection string
+        public const string CONNECTION_STRING = "Data Source=database.db";
+
+        public const string APP_NAME = "MyLibrary";
+        public const string APP_VERSION = "0.1.0";
+    }
+}
