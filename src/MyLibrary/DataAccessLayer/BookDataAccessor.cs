0b9e2eb74dafb45be473942da8563330884324de
Alc2110 <38523363+Alc2110@users.noreply.github.com>
2021-11-30 23:28:36 +1100

    trying to fix screw up

---
+++ src/MyLibrary/DataAccessLayer/BookDataAccessor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.Sqlite;
+using System.Data.SQLite;
+using MyLibrary.Models.Entities;
+
+namespace MyLibrary.DataAccessLayer
+{
+    public class BookDataAccessor
+    {
+        public async void Create(Book toAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Book> ReadById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Retrieve all Books from the database.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<Book>> ReadAll()
+        {
+            using (var conn = new SQLiteConnection(Configuration.CONNECTION_STRING))
+            {
+                return await conn.QueryAsync<Book>("SELECT * FROM Books;");
+            }
+        }
+
+        public async void Update(Book toUpdate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
