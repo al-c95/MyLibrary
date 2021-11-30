ed32fa4ee59265fe28d7d2a3caa854d8e825fcc6
Alc2110 <38523363+Alc2110@users.noreply.github.com>
2021-11-30 23:01:09 +1100

    first go at reading from the database

---
+++ src/MyLibrary/DataAccessLayer/MediaItemDataAccessor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibrary.Models.Entities;
+
+namespace MyLibrary.DataAccessLayer
+{
+    public class MediaItemDataAccessor
+    {
+        public async void Create(MediaItem toAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<MediaItem> ReadById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<MediaItem>> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void Update(MediaItem toUpdate)
+
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/MyLibrary/MainWindow.cs
+++ src/MyLibrary/MainWindow.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MyLibrary.Models.Entities;
+using MyLibrary.DataAccessLayer;
 
 namespace MyLibrary
 {
@@ -20,7 +22,25 @@ namespace MyLibrary
 
             // register event handlers
             this.exitMenuItem.Click += ((sender, args) => Application.Exit());
-            this.addButton.Click += ((sender, args) => new AddNewBookForm().ShowDialog());
+            this.addButton.Click += ((sender, args) => new AddNewMediaItemForm().ShowDialog());
         }//ctor
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            // get data
+            BookDataAccessor bookDataAccessor = new BookDataAccessor();
+            var allBooks = await bookDataAccessor.ReadAll();
+
+            // populate the data grid
+            this.dataGrid.Rows.Clear();
+            this.dataGrid.Columns.Clear();
+            this.dataGrid.Columns.Add("idCol", "Id");
+            this.dataGrid.Columns.Add("titleCol", "Title");
+            foreach (var book in allBooks)
+            {
+                object[] newRow = { book.Id, book.Title };
+                this.dataGrid.Rows.Add(newRow);
+            }
+        }
     }//class
 }
