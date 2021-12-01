61edf2ba41f2cc83a2c7b93948930f0fd517d59d
Alc2110 <38523363+Alc2110@users.noreply.github.com>
2021-12-01 22:42:31 +1100

    implement TagDataAccessor

--- src/MyLibrary/DataAccessLayer/TagDataAccessor.cs
+++ src/MyLibrary/DataAccessLayer/TagDataAccessor.cs
@@ -4,19 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MyLibrary.Models.Entities;
+using Dapper;
 
 namespace MyLibrary.DataAccessLayer
 {
+    /// <summary>
+    /// Database interface for creating, reading and deleting tags.
+    /// </summary>
     public class TagDataAccessor : DataAccessor
     {
-        public Task Create(Tag tag)
+        /// <summary>
+        /// Create a new tag.
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <returns></returns>
+        public async Task Create(Tag tag)
         {
-            throw new NotImplementedException();
+            const string SQL = "INSERT INTO Tags(name) " +
+                "VALUES (@name);";
+
+            using (var conn = GetConnection())
+            {
+                await conn.ExecuteAsync(SQL, new { tag.Name });
+            }
         }
 
-        public Task DeleteById(int id)
+        /// <summary>
+        /// Read all tags from the database.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<Tag>> ReadAll()
         {
-            throw new NotImplementedException();
+            const string SQL = "SELECT * FROM Tags;";
+            return await GetConnection().QueryAsync<Tag>(SQL);
         }
-    }
+
+        /// <summary>
+        /// Delete a tag by its id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task DeleteById(int id)
+        {
+            const string SQL = "DELETE FROM Tags WHERE id = @id;";
+
+            using (var conn = GetConnection())
+            {
+                await conn.ExecuteAsync(SQL, new { id });
+            }
+        }//DeleteByIdAsync
+    }//class
 }
