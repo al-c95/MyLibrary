6a3af09ea2d134c5c283ac90b8866f3c33a27d6c
al-c95 <137837298+al-c95@users.noreply.github.com>
2021-12-01 22:24:36 +1100

    refactor data accessors

---
+++ src/MyLibrary/DataAccessLayer/DataAccessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace MyLibrary.DataAccessLayer
+{
+    public abstract class DataAccessor
+    {
+        protected SQLiteConnection GetConnection()
+        {
+            return new SQLiteConnection(Configuration.CONNECTION_STRING);
+        }
+    }
+}
