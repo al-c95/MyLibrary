3fdf688f87eca65c3da00db7b0396577d68f4ca0
al-c95 <137837298+al-c95@users.noreply.github.com>
2021-12-05 22:47:48 +1100

    create skeleton for ItemPresenter tests

---
+++ src/MyLibrary_Test/Presenters_Tests/ItemPresenter_Tests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using NUnit;
+using NUnit.Framework;
+using FakeItEasy;
+using MyLibrary.BusinessLogic;
+using MyLibrary.Models.Entities;
+using MyLibrary.DataAccessLayer;
+
+namespace MyLibrary_Test.Presenters_Tests
+{
+    [TestFixture]
+    public class ItemPresenter_Tests
+    {
+        [Test]
+        public void CategorySelectionChanged_Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void ItemSelectionChanged_Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void PerformFilter_Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void DeleteButtonClicked_Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void UpdateSelectedItemButtonClicked_Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void SelectedItemModified_Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void DiscardSelectedItemChangesButtonClicked_Test()
+        {
+            throw new NotImplementedException();
+        }
+    }//class
+}
