00a776d0581334eb4069b5812cb6da0288fa5bd6
Alc2110 <38523363+Alc2110@users.noreply.github.com>
2021-12-09 22:56:56 +1100

    add tests for BookBuilder

---
+++ src/MyLibrary_Test/Models_Tests/Entities_Tests/Builders_Tests/BookBuilder_Tests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NUnit;
+using NUnit.Framework;
+using MyLibrary.Models.Entities;
+using MyLibrary.Models.Entities.Builders;
+
+namespace MyLibrary_Test.Models_Tests.Entities_Tests.Builders_Tests
+{
+    [TestFixture]
+    public class BookBuilder_Tests
+    {
+        [Test]
+        public void WithIsbn_Test_10()
+        {
+            // arrange/act
+            Book book = BookBuilder.CreateBook("test", "test book")
+                .WithIsbn("0123456789")
+                    .Get();
+
+            // assert
+            Assert.AreEqual("0123456789", book.Isbn);
+        }
+
+        [Test]
+        public void WithIsbn_Test_13()
+        {
+            // arrange/act
+            Book book = BookBuilder.CreateBook("test", "test book")
+                .WithIsbn("0123456789012")
+                    .Get();
+
+            // assert
+            Assert.AreEqual("0123456789012", book.Isbn13);
+        }
+
+        [TestCase("0")]
+        [TestCase("01234567890")]
+        [TestCase("01234567890123")]
+        public void WithIsbn_Test_IncorrectNumberOfDigits(string isbn)
+        {
+            Assert.Throws<ArgumentException>(() => BookBuilder.CreateBook("test", "test book")
+                .WithIsbn(isbn)
+                    .Get());
+        }
+        
+        [Test]
+        public void WithTags_Test()
+        {
+            // arrange/acts
+            List<Tag> tags = new List<Tag>
+            {
+                new Tag
+                {
+                    Name = "tag1"
+                },
+                new Tag
+                {
+                    Name = "tag2"
+                }
+            };
+            Book book = BookBuilder.CreateBook("test", "test book")
+                .WithTags(tags)
+                    .Get();
+            var tagsResult = (List<Tag>)(book.Tags);
+
+            // assert
+            Assert.IsTrue(tagsResult.Count == 2);
+            Assert.IsTrue(tagsResult.Any(t => t.Name == "tag1"));
+            Assert.IsTrue(tagsResult.Any(t => t.Name == "tag1"));
+        }
+    }//class
+}
