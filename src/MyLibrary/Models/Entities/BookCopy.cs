faf6b00bcf378ac7a88a464b462686cf77789e58
Alc2110 <38523363+Alc2110@users.noreply.github.com>
2022-01-29 18:02:13 +1100

    remove old code

--- src/MyLibrary/Models/Entities/BookCopy.cs
+++ src/MyLibrary/Models/Entities/BookCopy.cs
@@ -29,9 +29,5 @@ namespace MyLibrary.Models.Entities
     public sealed class BookCopy : Copy
     {
         public int BookId { get; set; }
-        /*
-        public string Description { get; set; }
-        public string Notes { get; set; }
-        */
     }
 }
