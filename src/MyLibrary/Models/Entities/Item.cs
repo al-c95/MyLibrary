19f1668265a403c96999a6ce975b7b3112a33ae4
Alc2110 <38523363+Alc2110@users.noreply.github.com>
2022-02-01 22:26:11 +1100

    refactor Item class hierarchy

--- src/MyLibrary/Models/Entities/Item.cs
+++ src/MyLibrary/Models/Entities/Item.cs
@@ -32,30 +32,11 @@ namespace MyLibrary.Models.Entities
     /// <summary>
     /// Base class for book or media item that can be stored in the database.
     /// </summary>
-    public abstract class Item : Entity // abstract class to reduce code duplication of title and image properties
+    public abstract class Item : ItemBase
     {
-        public abstract ItemType Type { get; set; }
-
         public Item()
         { 
             this.Tags = new List<Tag>(); 
-        }
-
-        private string _title;
-        public string Title
-        {
-            get => this._title;
-            set
-            {
-                if (value == null || string.IsNullOrWhiteSpace(value))
-                {
-                    throw new ArgumentNullException("Can't have an empty title.");
-                }
-                else
-                {
-                    _title = value;
-                }
-            }
         }
 
         public byte[] Image { get; set; } // conversion to and from images is handled by the presentation layer (view)
