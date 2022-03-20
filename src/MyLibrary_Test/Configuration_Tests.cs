5254acde20baff5f6e2e43a369c8e72db5dc9cb4
al-c95 <137837298+al-c95@users.noreply.github.com>
2022-03-21 00:02:44 +1100

    update tests for configuration and app version classes

--- src/MyLibrary_Test/Configuration_Tests.cs
+++ src/MyLibrary_Test/Configuration_Tests.cs
@@ -18,7 +18,10 @@ namespace MyLibrary_Test
                 "Thanks To: " + "\r\n" +
                 "Newtonsoft.Json 13.0.1 by James Newton-King" + "\r\n" +
                 "Dapper 2.0.123 by Sam Saffron, Marc Gravell and Nick Craver" + "\r\n" +
-                "Microsoft.Data.Sqlite.Core by Microsoft" + "\r\n";
+                "CircularProgressBar 2.8.0.16 by Soroush Falahati" + "\r\n" +
+                "WinFormAnimation 1.6.0.4 by Soroush Falahati" + "\r\n" +
+                "Microsoft.Data.Sqlite.Core by Microsoft" + "\r\n" +
+                "EntityFramework 6.4.4 by Microsoft\r\n";
 
             // act
             string actualResult = Configuration.APP_DESCRIPTION;
