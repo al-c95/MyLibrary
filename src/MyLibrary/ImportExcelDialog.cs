e386fedb7dc49d5f2872b99452a221ea0106d919
al-c95 <137837298+al-c95@users.noreply.github.com>
2022-08-06 22:07:00 +1000

    design import Excel dialog

---
+++ src/MyLibrary/ImportExcelDialog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyLibrary
+{
+    public partial class ImportExcelDialog : Form
+    {
+        public ImportExcelDialog()
+        {
+            InitializeComponent();
+
+            this.itemsList.View = View.Details;
+        }
+    }
+}
